286b696b235453cb9bea5a5af450c6d45d4b495f
mironbanjac <97656270+mironbanjac@users.noreply.github.com>
2024-05-07 14:52:21 +0200

    Update ds.vertCor.R

--- R/ds.vertCor.R
+++ R/ds.vertCor.R
@@ -1,40 +1,24 @@
-#' Client function for vertCor for finding correlation of vertically partitioned data in DataSHIELD
-#'
-#' @param variable_names names of variables to be used in correlation matrix
-#' @param datasources conns for the defined study
-#'
-#' @return correlation matrix
-#' @export
-#'
-#' @examples TODO
-ds.vertCor <- function(variable_names, datasources = NULL) {
+ds.vertCor <- function(data_names, variable_names, datasources = NULL) {
   # standard, if NULL look for connections
   if(is.null(datasources)){
     datasources <- datashield.connections_find()
   }
 
-  study_names  <- names(datasources)
-
-  calltext <- call("vertCorDS", variable_names)
-
-  output <- DSI::datashield.aggregate(datasources, calltext)
-
   U_combined <- NULL
-  #print(output)
 
-  for(i in 1:length(study_names)){
-    result <- output
-    #print(output)
-    U_combined <- cbind(U_combined, result)
+  study_names  <- names(datasources)
+  for(i in 1:length(study_names)) {
+    temp <-variable_names[[i]]
+    assign("temp", temp, envir = .GlobalEnv)
+    output <- datashield.aggregate(datasources[i], quote(vertCorDS(D, temp)))[[1]]
+    U_combined <- cbind(U_combined, output)
   }
 
 
   # Perform SVD on the combined U matrix
   svd_final <- svd(U_combined)
-
   # Calculate and scale the correlation matrix
   correlation <- svd_final$v %*% diag(svd_final$d^2) %*% t(svd_final$v)
   correlation <- correlation / correlation[1, 1]
-
   return(correlation)
 }
