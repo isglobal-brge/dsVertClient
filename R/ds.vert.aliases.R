07e06feabc63ea99596d6b34db2c942124fb8df8
David Sarrat González <113605621+davidsarrat@users.noreply.github.com>
2026-05-05 22:08:18 +0200

    feat: add adaptive product frontdoor routing

--- R/ds.vert.aliases.R
+++ R/ds.vert.aliases.R
@@ -35,6 +35,42 @@ NULL
   .dsvert_set_frontdoor(x, frontdoor, route = route, K = K)
 }
 
+.dsvert_arg_family <- function(args, default = "gaussian") {
+  family <- args$family %||% default
+  if (is.character(family) && length(family) >= 1L) return(family[[1L]])
+  if (inherits(family, "family") && is.character(family$family)) {
+    return(family$family[[1L]])
+  }
+  default
+}
+
+.dsvert_precision_intervals <- function() {
+  as.integer(getOption("dsvert.frontdoor_binomial_sigmoid_intervals", 150L))
+}
+
+.dsvert_apply_binomial_precision <- function(args,
+                                             precision = c("auto", "high",
+                                                           "fast"),
+                                             family = NULL,
+                                             force_binomial = FALSE) {
+  precision <- match.arg(precision)
+  if (identical(precision, "fast")) return(args)
+  if (is.null(family)) family <- .dsvert_arg_family(args)
+  if ((isTRUE(force_binomial) || identical(family, "binomial")) &&
+      is.null(args$binomial_sigmoid_intervals)) {
+    args$binomial_sigmoid_intervals <- .dsvert_precision_intervals()
+  }
+  args
+}
+
+.dsvert_add_policy <- function(x, method = NULL, precision = NULL) {
+  if (is.list(x)) {
+    if (!is.null(method)) x$method_frontdoor <- method
+    if (!is.null(precision)) x$precision_frontdoor <- precision
+  }
+  x
+}
+
 #' @rdname ds.vert.aliases
 #' @export
 ds.vert.align <- function(data_name, id_col, newobj = "D_aligned",
@@ -121,22 +157,41 @@ ds.vert.chisq_cross <- function(data, var1, var2,
 
 #' @rdname ds.vert.aliases
 #' @export
-ds.vert.glm <- function(formula, data = NULL, datasources = NULL, ...) {
+ds.vert.glm <- function(formula, data = NULL,
+                        precision = c("auto", "high", "fast"),
+                        datasources = NULL, ...) {
+  precision <- match.arg(precision)
   datasources <- .dsvert_datasources(datasources)
-  out <- ds.vertGLM(formula = formula, data = data,
-                    datasources = datasources, ...)
-  .dsvert_set_frontdoor(out, "ds.vert.glm", "ds.vertGLM",
-                        length(datasources))
+  args <- .dsvert_apply_binomial_precision(
+    c(list(formula = formula, data = data, datasources = datasources),
+      list(...)),
+    precision = precision)
+  out <- do.call(ds.vertGLM, args)
+  out <- .dsvert_set_frontdoor(out, "ds.vert.glm", "ds.vertGLM",
+                               length(datasources))
+  .dsvert_add_policy(out, precision = precision)
 }
 
 #' @rdname ds.vert.aliases
 #' @export
-ds.vert.cox <- function(formula, data = NULL, datasources = NULL, ...) {
+ds.vert.cox <- function(formula, data = NULL,
+                        method = c("profile", "discrete"),
+                        datasources = NULL, ...) {
+  method <- match.arg(method)
   datasources <- .dsvert_datasources(datasources)
-  out <- ds.vertCox(formula = formula, data = data,
-                    datasources = datasources, ...)
-  .dsvert_set_frontdoor(out, "ds.vert.cox", "ds.vertCox",
-                        length(datasources))
+  if (identical(method, "profile")) {
+    out <- ds.vertCox(formula = formula, data = data,
+                      datasources = datasources, ...)
+    route <- "ds.vertCoxProfileNonDisclosive"
+  } else {
+    out <- ds.vertCoxDiscreteNonDisclosive(
+      formula = formula, data = data, target = "discrete_logit",
+      datasources = datasources, ...)
+    route <- "ds.vertCoxDiscreteNonDisclosive"
+  }
+  out <- .dsvert_set_frontdoor(out, "ds.vert.cox", route,
+                               length(datasources))
+  .dsvert_add_policy(out, method = method)
 }
 
 #' @rdname ds.vert.aliases
@@ -150,18 +205,18 @@ ds.vert.coxph <- function(formula, data = NULL, ...) {
 #' @rdname ds.vert.aliases
 #' @export
 ds.vert.nb <- function(formula, data = NULL,
-                       method = c("auto", "fast", "mom", "profile",
-                                  "accurate"),
+                       method = c("auto", "accurate", "fast", "mom",
+                                  "profile"),
                        datasources = NULL, ...) {
   method <- match.arg(method)
   datasources <- .dsvert_datasources(datasources)
   K <- length(datasources)
   route <- switch(method,
-    auto = "ds.vertNBMoMTheta",
+    auto = "ds.vertNBFullRegTheta",
+    accurate = "ds.vertNBFullRegTheta",
     fast = "ds.vertNBMoMTheta",
     mom = "ds.vertNBMoMTheta",
-    profile = "ds.vertNB",
-    accurate = "ds.vertNBFullRegTheta")
+    profile = "ds.vertNB")
   fit <- switch(route,
     ds.vertNBMoMTheta = ds.vertNBMoMTheta(
       formula = formula, data = data, datasources = datasources, ...),
@@ -170,7 +225,8 @@ ds.vert.nb <- function(formula, data = NULL,
     ds.vertNBFullRegTheta = ds.vertNBFullRegTheta(
       formula = formula, data = data, variant = "full_reg_nd",
       datasources = datasources, ...))
-  .dsvert_set_frontdoor(fit, "ds.vert.nb", route, K)
+  fit <- .dsvert_set_frontdoor(fit, "ds.vert.nb", route, K)
+  .dsvert_add_policy(fit, method = method)
 }
 
 #' @rdname ds.vert.aliases
@@ -234,34 +290,43 @@ ds.vert.lmm <- function(formula, data = NULL, cluster_col,
 
 #' @rdname ds.vert.aliases
 #' @export
-ds.vert.gee <- function(formula, data = NULL, datasources = NULL, ...) {
+ds.vert.gee <- function(formula, data = NULL,
+                        precision = c("auto", "high", "fast"),
+                        datasources = NULL, ...) {
+  precision <- match.arg(precision)
   datasources <- .dsvert_datasources(datasources)
-  out <- ds.vertGEE(formula = formula, data = data,
-                    datasources = datasources, ...)
-  .dsvert_set_frontdoor(out, "ds.vert.gee", "ds.vertGEE",
-                        length(datasources))
+  args <- .dsvert_apply_binomial_precision(
+    c(list(formula = formula, data = data, datasources = datasources),
+      list(...)),
+    precision = precision)
+  out <- do.call(ds.vertGEE, args)
+  out <- .dsvert_set_frontdoor(out, "ds.vert.gee", "ds.vertGEE",
+                               length(datasources))
+  .dsvert_add_policy(out, precision = precision)
 }
 
 #' @rdname ds.vert.aliases
 #' @export
 ds.vert.glmm <- function(formula, data = NULL, cluster_col,
-                         method = c("laplace", "pql"),
+                         method = c("auto", "laplace", "pql"),
                          datasources = NULL, ...) {
   method <- match.arg(method)
+  route_method <- if (identical(method, "auto")) "laplace" else method
   datasources <- .dsvert_datasources(datasources)
-  if (identical(method, "laplace")) {
+  if (identical(route_method, "laplace")) {
     out <- ds.vertGLMMLaplace(formula = formula, data = data,
                               cluster_col = cluster_col,
                               datasources = datasources, ...)
-    .dsvert_set_frontdoor(out, "ds.vert.glmm", "ds.vertGLMMLaplace",
-                          length(datasources))
+    out <- .dsvert_set_frontdoor(out, "ds.vert.glmm", "ds.vertGLMMLaplace",
+                                 length(datasources))
   } else {
     out <- ds.vertGLMM(formula = formula, data = data,
                        cluster_col = cluster_col,
                        datasources = datasources, ...)
-    .dsvert_set_frontdoor(out, "ds.vert.glmm", "ds.vertGLMM",
-                          length(datasources))
+    out <- .dsvert_set_frontdoor(out, "ds.vert.glmm", "ds.vertGLMM",
+                                 length(datasources))
   }
+  .dsvert_add_policy(out, method = method)
 }
 
 #' @rdname ds.vert.aliases
@@ -279,13 +344,21 @@ ds.vert.glmer <- function(formula, data = NULL, cluster_col,
 #' @rdname ds.vert.aliases
 #' @export
 ds.vert.ipw <- function(outcome_formula, propensity_formula, data = NULL,
+                        precision = c("auto", "high", "fast"),
                         datasources = NULL, ...) {
+  precision <- match.arg(precision)
   datasources <- .dsvert_datasources(datasources)
-  out <- ds.vertIPW(outcome_formula = outcome_formula,
-                    propensity_formula = propensity_formula,
-                    data = data, datasources = datasources, ...)
-  .dsvert_set_frontdoor(out, "ds.vert.ipw", "ds.vertIPW",
-                        length(datasources))
+  args <- .dsvert_apply_binomial_precision(
+    c(list(outcome_formula = outcome_formula,
+           propensity_formula = propensity_formula,
+           data = data, datasources = datasources),
+      list(...)),
+    precision = precision,
+    force_binomial = TRUE)
+  out <- do.call(ds.vertIPW, args)
+  out <- .dsvert_set_frontdoor(out, "ds.vert.ipw", "ds.vertIPW",
+                               length(datasources))
+  .dsvert_add_policy(out, precision = precision)
 }
 
 #' @rdname ds.vert.aliases
@@ -309,9 +382,22 @@ ds.vert.lasso <- function(fit, lambda_1, ...) {
 
 #' @rdname ds.vert.aliases
 #' @export
-ds.vert.lasso_iter <- function(formula, data = NULL, ...) {
-  .dsvert_route_result(ds.vertLASSOIter(formula = formula, data = data, ...),
-                       "ds.vert.lasso_iter", "ds.vertLASSOIter")
+ds.vert.lasso_iter <- function(formula, data = NULL,
+                               method = c("auto", "accurate", "fast"),
+                               ...) {
+  method <- match.arg(method)
+  args <- c(list(formula = formula, data = data), list(...))
+  if (is.null(args$exact_non_gaussian)) {
+    args$exact_non_gaussian <- !identical(method, "fast")
+  }
+  route <- if (isTRUE(args$exact_non_gaussian)) {
+    "ds.vertLASSOIter(aggregate-score)"
+  } else {
+    "ds.vertLASSOIter(one-step-surrogate)"
+  }
+  out <- .dsvert_route_result(do.call(ds.vertLASSOIter, args),
+                              "ds.vert.lasso_iter", route)
+  .dsvert_add_policy(out, method = method)
 }
 
 #' @rdname ds.vert.aliases
